ff4eb9506f1bf804f44f9147d1e1549ff4371688
Cody Tilkins <deocojo1@gmail.com>
2025-01-14 03:22:31 -0500

    Fix time sensitive exploit as referenced in https://gruss.cc/files/counterseveillance.pdf (thank you)

--- cotp.c
+++ cotp.c
@@ -124,43 +124,29 @@ void otp_free(OTPData* data)
 			1 success
 		error, 0
 */
-COTPRESULT otp_byte_secret(OTPData* data, char* out_str)
-{
-	if (out_str == NULL || strlen(data->base32_secret) % 8 != 0)
+COTPRESULT otp_byte_secret(OTPData* data, char* out_str) {
+	if (out_str == NULL || strlen(data->base32_secret) % 8 != 0) {
 		return OTP_ERROR;
+	}
 	
 	size_t base32_length = strlen(data->base32_secret);
 	size_t num_blocks = base32_length / 8;
 	size_t output_length = num_blocks * 5;
 	
-	if (output_length == 0)
+	if (output_length == 0) {
 		return OTP_OK;
+	}
 	
-	for (size_t i = 0; i < num_blocks; i++)
-	{
+	int valid = 1;
+	
+	for (size_t i = 0; i < num_blocks; i++) {
 		unsigned int block_values[8] = { 0 };
 		
-		for (int j = 0; j < 8; j++)
-		{
+		for (int j = 0; j < 8; j++) {
 			char c = data->base32_secret[i * 8 + j];
-			if (c == '=')
-				break;
-				
-			int found = 0;
-			for (int k = 0; k < 32; k++)
-			{
-				if (c == OTP_DEFAULT_BASE32_CHARS[k])
-				{
-					block_values[j] = k;
-					found = 1;
-					break;
-				}
-			}
-			
-			if (!found)
-			{
-				return OTP_ERROR;
-			}
+			unsigned int value = (unsigned char) c < 256 ? OTP_DEFAULT_BASE32_OFFSETS[(unsigned char) c] : -1;
+			block_values[j] = value & 31;
+			valid &= (value >= 0);
 		}
 		
 		out_str[i * 5] = (block_values[0] << 3) | (block_values[1] >> 2);
@@ -169,8 +155,8 @@ COTPRESULT otp_byte_secret(OTPData* data, char* out_str)
 		out_str[i * 5 + 3] = (block_values[4] << 7) | (block_values[5] << 2) | (block_values[6] >> 3);
 		out_str[i * 5 + 4] = (block_values[6] << 5) | block_values[7];
 	}
-	
-	return OTP_OK;
+
+	return valid ? OTP_OK : OTP_ERROR;
 }
 
 /*
--- test/main.c
+++ test/main.c
@@ -226,8 +226,8 @@ int main(int argc, char** argv)
 	char base32_new_secret[base32_len + 1];
 	memset(&base32_new_secret, 0, base32_len + 1);
 	
-	otp_random_base32(base32_len, base32_new_secret);
-	printf("Random Generated BASE32 Secret: `%s`\n", base32_new_secret);
+	int random_otp_err = otp_random_base32(base32_len, base32_new_secret);
+	printf("Random Generated BASE32 Secret pass=1: `%s` `%d`\n", base32_new_secret, random_otp_err);
 	
 	puts(""); // line break for readability
 	
